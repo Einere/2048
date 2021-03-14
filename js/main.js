0ade78c19440ada9eb18740298e9a521af28babc
Einere <kjwsx23@gmail.com>
2021-03-14 20:11:57 +0900

    feat: implement get direction
    드래그 방향을 얻는 로직을 추가함.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const fxjs = window._;
 const {go, range, forEach, map, zipWithIndexL, filter, flat} = fxjs;
 const table = document.getElementById('table');
-const data = [];
+const tableData = [];
 const dimension = 4;
 
 function init() {
@@ -23,7 +23,7 @@ function init() {
         })
       )
 
-      data.push(rowData);
+      tableData.push(rowData);
       fragment.appendChild(tr);
     })
   )
@@ -32,10 +32,10 @@ function init() {
 
 function randomGenerate() {
   const filteredIndexedData = go(
-    data,
+    tableData,
     zipWithIndexL,
     map(([i, row]) => go(
-      data[i],
+      tableData[i],
       zipWithIndexL,
       map(([j, column]) => [i, j, column])
     )),
@@ -43,26 +43,22 @@ function randomGenerate() {
     filter(indexedData => indexedData[2] === 0)
   );
 
-  console.log(filteredIndexedData);
-
   const randomIndex = Math.floor(Math.random() * filteredIndexedData.length);
   const [i, j] = filteredIndexedData[randomIndex];
-  data[i][j] = 2;
+  tableData[i][j] = 2;
 
   render();
 }
 
 function render() {
   go(
-    data,
+    tableData,
     zipWithIndexL,
     forEach(([i, row]) => {
-      console.log('row', i, row);
       go(
-        data[i],
+        tableData[i],
         zipWithIndexL,
         forEach(([j, column]) => {
-          console.log('column', j, column);
           table.children[i].children[j].textContent = column === 0 ? '' : column;
           // table.children[i].children[j].textContent = column;
         })
@@ -76,3 +72,89 @@ init();
 render();
 randomGenerate();
 
+// 드래그 방향을 계산
+// 인강에서는 delta 값의 양음 여부와 기울기로 판단.
+let isMouseClicked = false;
+let isMouseMoved = false;
+let startCoordinate = null;
+let endCoordinate = null;
+const Direction = {
+  RIGHT: 'RIGHT',
+  UP: 'UP',
+  LEFT: 'LEFT',
+  DOWN: 'DOWN',
+};
+
+window.addEventListener('mousedown', (e) => {
+  isMouseClicked = true;
+  startCoordinate = [e.clientX, e.clientY];
+});
+
+window.addEventListener('mousemove', (e) => {
+  if(!isMouseClicked) {
+    isMouseMoved = true;
+    return;
+  }
+});
+
+function getTheta (deltaX, deltaY) {
+  // 데카르트 좌표계 -> 각 좌표계
+  // https://ko.wikipedia.org/wiki/%EA%B7%B9%EC%A2%8C%ED%91%9C%EA%B3%84#%EA%B7%B9%EC%A2%8C%ED%91%9C%EC%99%80_%EB%8D%B0%EC%B9%B4%EB%A5%B4%ED%8A%B8_%EC%A2%8C%ED%91%9C_%EC%82%AC%EC%9D%B4%EC%9D%98_%EB%B3%80%ED%99%98
+  if(deltaX > 0 && deltaY >= 0) {
+    return Math.atan(deltaY / deltaX);
+  }
+  else if (deltaX > 0 && deltaY < 0) {
+    return Math.atan(deltaY / deltaX) + 2 * Math.PI;
+  }
+  else if (deltaX < 0) {
+    return Math.atan(deltaY / deltaX) + Math.PI;
+  }
+  else if (deltaX === 0 && deltaY > 0) {
+    return Math.PI / 2;
+  }
+  else if (deltaX === 0 && deltaY < 0) {
+    return Math.PI * 3 / 2;
+  }
+}
+
+function getDirection(startCoordinate, endCoordinate) {
+  const deltaX = endCoordinate[0] - startCoordinate[0];
+  // 브라우저에서 y는 아래로 갈수록 +의 무한대에 가까워지기 때문에, 데카르트 좌표계로 조건을 판단하기 위해서는 뒤집어줘야 한다.
+  const deltaY = -(endCoordinate[1] - startCoordinate[1]);
+
+  const theta =  getTheta(deltaX, deltaY);
+  const isLower45 = theta < 1 / 4 * Math.PI;
+  const isUpper45 = theta > 1 / 4 * Math.PI;
+  const isLower135 = theta < 3 / 4 * Math.PI;
+  const isUpper135 = theta > 3 / 4 * Math.PI;
+  const isLower225 = theta < 5 / 4 * Math.PI;
+  const isUpper225 = theta > 5 / 4 * Math.PI;
+  const isLower315 = theta < 7 / 4 * Math.PI;
+  const isUpper315 = theta > 7 / 4 * Math.PI;
+
+  if(isUpper45 && isLower135) {
+    return Direction.UP;
+  }
+  else if (isUpper135 && isLower225) {
+    return Direction.LEFT;
+  }
+  else if (isUpper225 && isLower315) {
+    return Direction.DOWN;
+  }
+  else if (isUpper315 || isLower45) {
+    return Direction.RIGHT;
+  }
+}
+
+window.addEventListener('mouseup', (e) => {
+  endCoordinate = [e.clientX, e.clientY];
+
+  if(isMouseMoved) {
+    const direction = getDirection(startCoordinate, endCoordinate);
+
+    console.log(direction);
+  }
+
+  isMouseClicked = false;
+  isMouseMoved = false;
+});
